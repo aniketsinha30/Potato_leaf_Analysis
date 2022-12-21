e175bb67ac0f2c73d02bdf413bb54d9a16236f24
aniketsinha30 <asaniketsinha@gmail.com>
2022-12-22 01:18:38 +0530

    potato leaf Analysis

---
+++ API/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/ping")
+async def ping():
+    return "Hello, Aniket I am alive"
+
+if __name__=="__main__":
+    uvicorn.run(app,host='localhost',port=8000)
+    
